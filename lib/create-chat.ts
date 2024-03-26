2ad40b1e2a2c197cd7c059da7096bf11c7ed2559
Mahir1902 <mahirhaqe7@gmail.com>
2024-03-26 18:58:56 +1100

    minor update

--- lib/create-chat.ts
+++ lib/create-chat.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 export const createChat = async (videoLink:string) => {
-
+    console.log('Hit chat endpoint')
     const res = await axios.post(`${process.env.NEXT_PUBLIC_URL}/api/create-chat`, {videoLink})
     // console.log(res.data)
     return res.data
